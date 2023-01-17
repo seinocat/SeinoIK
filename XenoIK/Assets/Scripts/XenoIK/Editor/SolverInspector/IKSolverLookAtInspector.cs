a923c7babe56dc8487a353790b08e6f5783b0274
songqin <980047129@qq.com>
2023-01-17 09:44:19 +0800

    add curve

--- XenoIK/Assets/Scripts/XenoIK/Editor/SolverInspector/IKSolverLookAtInspector.cs
+++ XenoIK/Assets/Scripts/XenoIK/Editor/SolverInspector/IKSolverLookAtInspector.cs
@@ -6,12 +6,30 @@ namespace XenoIK.Editor
     public class IKSolverLookAtInspector : IKSolverInspector
     {
         private static MonoBehaviour script;
+        private static bool FadeFoldout = true;
         
         public static void DrawInspector(SerializedProperty prop, MonoBehaviour mono)
         {
             script = mono;
             EditorGUILayout.PropertyField(prop.FindPropertyRelative("target"), new GUIContent("注视目标"));
             EditorGUILayout.PropertyField(prop.FindPropertyRelative("IKWeight"), new GUIContent("全局权重"));
+
+            FadeFoldout = EditorGUILayout.Foldout(FadeFoldout, "配置相关");
+            if (FadeFoldout)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    GUILayout.BeginHorizontal();
+                    GUILayout.Space((EditorGUI.indentLevel + 1) * 15);
+                    if(i == 0)EditorGUILayout.PropertyField(prop.FindPropertyRelative("angle"), new GUIContent("有效角度"));
+                    if(i == 1)EditorGUILayout.PropertyField(prop.FindPropertyRelative("distance"), new GUIContent("有效距离"));
+                    if(i == 2)EditorGUILayout.PropertyField(prop.FindPropertyRelative("fadeInTime"), new GUIContent("渐入时间"));
+                    if(i == 3)EditorGUILayout.PropertyField(prop.FindPropertyRelative("fadeOutTime"), new GUIContent("渐出时间"));
+                    if(i == 4)EditorGUILayout.PropertyField(prop.FindPropertyRelative("fadeInCurve"), new GUIContent("渐入曲线"));
+                    if(i == 5)EditorGUILayout.PropertyField(prop.FindPropertyRelative("fadeOutCurve"), new GUIContent("渐出曲线"));
+                    GUILayout.EndHorizontal();
+                }
+            }
             
             EditorGUILayout.PropertyField(prop.FindPropertyRelative("head.transform"), new GUIContent("头骨骼"));
             DrawElements(prop.FindPropertyRelative("eyes"), 0, new GUIContent("眼睛"), DrawLookAtBone, DrawEyeBtns);
--- XenoIK/Assets/Scripts/XenoIK/Scripts/IKSolvers/IKSolverLookAt.cs
+++ XenoIK/Assets/Scripts/XenoIK/Scripts/IKSolvers/IKSolverLookAt.cs
@@ -12,6 +12,17 @@ namespace XenoIK
         public List<LookAtBone> eyes;
         public List<LookAtBone> spines;
 
+        [Range(0, 360)]
+        public float angle;
+        [Range(0, float.MaxValue)]
+        public float distance;
+        [Range(0, float.MaxValue)]
+        public float fadeInTime;
+        [Range(0, float.MaxValue)]
+        public float fadeOutTime;
+        public AnimationCurve fadeInCurve = new AnimationCurve(new Keyframe(0,0,1,1), new Keyframe(1,1,1,1));
+        public AnimationCurve fadeOutCurve = new AnimationCurve(new Keyframe(0,0,1,1), new Keyframe(1,1,1,1));
+
 
         protected override void OnInitialize()
         {
