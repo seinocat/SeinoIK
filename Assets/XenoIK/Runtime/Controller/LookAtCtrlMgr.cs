331a97c08f139146d1d35476b23f1c7d19158cdf
songqin <980047129@qq.com>
2023-02-01 14:06:58 +0800

    add LookAtCtrlMgr.cs

---
+++ Assets/XenoIK/Runtime/Controller/LookAtCtrlMgr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace XenoIK
+{
+    public class LookAtCtrlMgr : MonoBehaviour
+    {
+        public List<LookAtCtrl> ctrls;
+
+        private void Awake()
+        {
+            this.ctrls = this.transform.GetComponentsInChildren<LookAtCtrl>().ToList();
+        }
+        
+        public void OpenIK()
+        {
+            this.ctrls?.ForEach(ctrl=>ctrl.OpenIK());
+        }
+        
+        public void CloseIK()
+        {
+            this.ctrls?.ForEach(ctrl=>ctrl.CloseIK());
+        }
+    }
+}
