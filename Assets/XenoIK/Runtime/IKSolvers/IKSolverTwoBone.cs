99f0c126972f5438c1b196f325cb0a13216baea6
songqin <980047129@qq.com>
2023-02-08 18:06:56 +0800

    add twobone

---
+++ Assets/XenoIK/Runtime/IKSolvers/IKSolverTwoBone.cs
@@ -0,0 +1,25 @@
+﻿namespace XenoIK
+{
+    public class IKSolverTwoBone : IKSolver
+    {
+        protected override void OnInitialize()
+        {
+            
+        }
+
+        protected override void OnUpdate(float deltaTime)
+        {
+            
+        }
+
+        public override void StoreDefaultLocalState()
+        {
+            
+        }
+
+        public override void FixTransform()
+        {
+            
+        }
+    }
+}
---
+++ Assets/XenoIK/Runtime/Mono/TwoBoneIK.cs
@@ -0,0 +1,12 @@
+﻿namespace XenoIK
+{
+    public class TwoBoneIK : XenoIK
+    {
+        public IKSolverTwoBone solver = new IKSolverTwoBone();
+        
+        protected override IKSolver GetIKSolver()
+        {
+            return solver;
+        }
+    }
+}
