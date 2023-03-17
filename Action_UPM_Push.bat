c7f2cf27155dabb6845725d3a2b59cc9f7653021
songqin <980047129@qq.com>
2023-03-17 18:32:45 +0800

    移除bat命令

--- Action_UPM_Push.bat
+++
@@ -1,2 +0,0 @@
-git subtree push --prefix=Assets/XenoIK https://github.com/Svily/XenoIK.git upm
-git subtree push --prefix=Assets/XenoIK http://192.168.6.23:8061/songqin/XenoIK.git upm
--- Action_UPM_Rebuild.bat
+++
@@ -1,4 +0,0 @@
-git branch -D upm
-git push origin -d upm
-git subtree split --prefix=Assets/XenoIK --branch upm
-git push origin upm --tags
