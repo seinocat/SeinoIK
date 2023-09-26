46fdc631c0a1896bc11ebf038e35160cf65e1cd4
songqin <980047129@qq.com>
2023-09-26 17:45:17 +0800

    feat:增加控制器

---
+++ Runtime/Controller/FootIKCtrl.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using XenoIK.Runtime.Ground;
+
+namespace XenoIK
+{
+    public class FootIKCtrl : MonoBehaviour
+    {
+        public List<FootIK> FootIks;
+    }
+}
